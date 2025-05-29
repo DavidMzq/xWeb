2525252a5dfcd853bafb430459eba2b92f13018d
David MA <Zhiqiang_David_MA@baxter.com>
2025-05-29 12:28:14 -0700

    update for moving node_modules folder out

---
+++ x-tictac/build/precache-manifest.80dad04d315acffd62a1c6d60d5f1c82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3c4dafd767cac7ae57029bcc02696c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "757110c0c96568352d81",
+    "url": "/static/css/main.088ea9f6.chunk.css"
+  },
+  {
+    "revision": "7fdab41193a363978660",
+    "url": "/static/js/2.fbdcf7f6.chunk.js"
+  },
+  {
+    "revision": "757110c0c96568352d81",
+    "url": "/static/js/main.e45aca0c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
