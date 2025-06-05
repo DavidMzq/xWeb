006709998b5719e29d19f95701660b3006a4c303
David MA <Zhiqiang_David_MA@baxter.com>
2025-06-05 13:33:05 -0700

    complete all for tictac- next https://react.dev/learn/thinking-in-react

--- x-react/src/App.js
+++ x-react/src/App.js
@@ -1,46 +1,45 @@
-import logo from './logo.svg';
-import {useState} from 'react';
-import './App.css';
+import logo from './logo.svg'
+import { useState } from 'react'
+import './App.css'
 
 const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://alexisplantin.fr/assets/images/prometheus-grafana/prometheus-grafana.png',
+  name: 'Code Test',
+  imageUrl:
+    'https://alexisplantin.fr/assets/images/prometheus-grafana/prometheus-grafana.png',
   imageSize_weight: 700,
-  imageSize_height: 180
-};
+  imageSize_height: 180,
+}
 
 const products = [
   { title: 'Cabbage', isFruit: false, id: 1 },
   { title: 'Garlic', isFruit: false, id: 2 },
   { title: 'Apple', isFruit: true, id: 3 },
-];
+]
 
 function ShoppingList() {
-  const listItems = products.map(product =>
+  const listItems = products.map((product) => (
     <li
       key={product.id}
       style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
+        color: product.isFruit ? 'magenta' : 'darkgreen',
       }}
     >
       {product.title}
     </li>
-  );
+  ))
 
-  return (
-    <ul>{listItems}</ul>
-  );
+  return <ul>{listItems}</ul>
 }
 
 function MyButton({ count, onClick }) {
   // const [count, setCount] = useState(0);
   return (
     <button onClick={onClick}>
-      I'm a button<br></br>
+      Button0<br></br>
       Clicked {count} times
       {/* I'm a button */}
     </button>
-  );
+  )
   // function handleClick() {
   //   setCount(count + 1);
   //   alert('You clicked me!');
@@ -48,61 +47,76 @@ function MyButton({ count, onClick }) {
 }
 
 function MyButton2() {
-  const [count2, setCount] = useState(0);
+  const [count2, setCount] = useState(0)
   return (
     <button onClick={handleClick}>
-      I'm a button<br></br>
+      Button1<br></br>
       Clicked {count2} times
       {/* I'm a button */}
     </button>
-  );
+  )
   function handleClick() {
-    setCount(count2 + 1);
-    alert('You clicked me!');
+    setCount(count2 + 1)
+    alert('You clicked me!')
   }
 }
 
 function App() {
-  const [count, setCount] = useState(0);
-  function handleClick() { 
-    setCount(count + 1); 
-    alert('You clicked me!');
-  } 
+  const [count, setCount] = useState(0)
+  const numbers = [1, 2, 3]
+  const doubled = numbers.map((num, index) => {
+    return num * 2 // This will double each number
+  })
+
+  function handleClick() {
+    setCount(count + 1)
+    alert('You clicked me!')
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <div>
+          <h3>Doubled Numbers</h3>
+          {doubled}
+          <p>{`[${doubled.join(', ')}]`}</p>
+          <ul>
+            {doubled.map((value, index) => (
+              <li key={index}>{value}</li> // Render each doubled value in a list item
+            ))}
+          </ul>
+
+        </div>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          links
         </a>
-
       </header>
       <div>
-      <h1>{user.name}</h1>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton2 />
-      <ShoppingList />
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize_weight,
-          height: user.imageSize_height 
-        }}
-        
-      />
-       </div>
+        <h1>{user.name}</h1>
+        <MyButton count={count} onClick={handleClick} />
+        <MyButton2 />
+        <ShoppingList />
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize_weight,
+            height: user.imageSize_height,
+          }}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- x-tictac/src/App.js
+++ x-tictac/src/App.js
@@ -73,36 +73,41 @@ function Board({ xIsNext, squares, onPlay }) {
   )
 }
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  // const [xIsNext, setXIsNext] = useState(true)
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const [currentMove, setCurrentMove] = useState(0)
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1);
+    // setXIsNext(!xIsNext)
   }
 
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove)
+    // setXIsNext(nextMove % 2 === 0)
   }
 
   const moves = history.map((squares, move) => {
-    let description;
+    let description
     if (move > 0) {
-      description = 'Go to move #' + move;
+      description = 'Go to move #' + move
     } else {
-      description = 'Go to game start';
+      description = 'Go to game start'
     }
     return (
       <li>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
-    );
-  });
+    )
+  })
   return (
     <div className="game">
       <div className="game-board">
-         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
@@ -110,4 +115,3 @@ export default function Game() {
     </div>
   )
 }
-
